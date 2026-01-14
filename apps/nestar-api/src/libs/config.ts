d8b043a54cf6aac52793c5b7a999dc5cf1bb1406
shox17 <159898318+shox17@users.noreply.github.com>
2026-01-14 13:10:04 +0900

    feat: develop admin related boardArticle GraphQL APIs

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -15,6 +15,8 @@ export const availablePropertySorts = [
 	'propertyPrice',
 ];
 
+export const availableBoardArticleSorts = ['createdAt', 'updatedAt', 'articleLikes', 'articleViews'];
+
 /** IMAGE CONFIGURATION **/
 export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
 export const getSerialForImage = (filename: string) => {
